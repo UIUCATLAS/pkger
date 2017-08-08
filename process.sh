5d8403697cc29e6a96fb2a38b977d772d6d9c69a
Pat Szuta <szuta@illinois.edu>
2017-08-08 15:44:41 -0500

    Update process.sh
    Changed single quotes to double quotes. Oops.

--- process.sh
+++ process.sh
@@ -4,8 +4,8 @@ key=$2
 version=$3
 echo $script
 echo $key
-sed -i 's/UIUCIDENTIFIER/$key/' /pkg/base.pkg/PackageInfo
-sed -i 's/UIUCVERSION/$version/' /pkg/base.pkg/PackageInfo
+sed -i "s/UIUCIDENTIFIER/$key/" /pkg/base.pkg/PackageInfo
+sed -i "s/UIUCVERSION/$version/" /pkg/base.pkg/PackageInfo
 cp /data/$script /pkg/scripts/postinstall
 chmod +x /pkg/scripts/postinstall
 (cd /pkg/scripts &&  find . | cpio -o --format odc --owner 0:80 | gzip -c ) > /pkg/base.pkg/Scripts
